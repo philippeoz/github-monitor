72c5e1f01554508c9f6968c1ab3ea14378f04551
Philippe Gonzaga <francisco.philippe@gmail.com>
2021-11-30 06:05:33 -0300

    add request method override (base_url and path join) and headers update

--- common/integrations/github.py
+++ common/integrations/github.py
@@ -6,3 +6,14 @@ from urllib.parse import urljoin
 class GitHub(requests.Session):
     """GitHub integration"""
     BASE_URL = 'https://api.github.com'
+
+    def __init__(self):
+        """Init override to update default_headers"""
+        super().__init__()
+        self.headers.update({"Accept": "application/vnd.github.v3+json"})
+
+    def request(self, method, path, **kwargs):
+        """Override request method to accept the url path"""
+        return super().request(
+            method, urljoin(GitHub.BASE_URL, path), **kwargs
+        )
