27f36b71cbf9f32c595dc93d1c23fcf809b6c37f
Philippe Gonzaga <francisco.philippe@gmail.com>
2021-12-01 15:23:39 -0300

    isort

--- repositories/urls.py
+++ repositories/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path, include
-
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import RepositoryViewSet, CommitViewSet
+from .views import CommitViewSet, RepositoryViewSet
 
 app_name = 'repositories'
 
